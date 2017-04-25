6d2e7785863af894c25313b63131de1251c19873
Unknown <s.gonzalezpons@gmail.com>
2017-04-25 21:11:39 +0200

    Flash y nave no sale
    La nave ya no sale de la escena por mucho que nos esforzemos, ademas de ir tan fluida como antes el movimiento. 
    He implementado un flash, usando la barra espaciadora, hacia la dirección en la que te estes moviendo (tambien diagonal) hace falta modificar alguna cosilla. Le he puesto particulas, estan comentadas hacia todas las direcciones menos hacia arriba

--- MOVIMIENTO NAVE/Assets/scripts/PLAYER_CONTROL.cs
+++ MOVIMIENTO NAVE/Assets/scripts/PLAYER_CONTROL.cs
@@ -1,29 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PLAYER_CONTROL : MonoBehaviour {
+public class PLAYER_CONTROL : MonoBehaviour
+{
     //CONTROL TECLAS APRETADAS
-    
+
     bool buttonPressed;
 
+    bool UpPressed = false;
+    bool DownPressed = false;
+    bool LeftPressed = false;
+    bool RightPressed = false;
     //VELOCIDAD MOVIMIENTO
     [Header("Movement")]
     public float GoUp = 40;
     public float GoDown = -40;
     public float GoLeft = -40;
     public float GoRigh = 40;
-
-    public float speed = 40;
-    public float finalSpeed;
-
+    public GameObject PLAYER;
     public float limiteX;
     public float limiteY;
+    public float Dash;
+    public GameObject flash;
+    
     //VELOCIDAD BOOST
     public float BoostUp = 40;
     public float BoostDown = -40;
     public float BoostLeft = -40;
     public float BoostRigh = 40;
-    
+
+
+    float posicionX;
+    float posicionY;
+
+    //FRENADA
+    float UpBreak = 20;
+    float DownBreak = -20;
+    float LeftBreak = -20;
+    float RightBreak = 20;
 
 
 
@@ -34,84 +48,76 @@ public class PLAYER_CONTROL : MonoBehaviour {
     public float fireRate;
     private float nextFire;
 
+
+
+
+
+
     // Use this for initialization
-    void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         buttonPressed = false;
 
+        posicionX = (PLAYER.transform.position.x);
+        posicionY = (PLAYER.transform.position.y);
+
+        if (posicionX > limiteX)
+        {
+            PLAYER.transform.position = new Vector3(limiteX, PLAYER.transform.position.y, PLAYER.transform.position.z);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
+            
+        }
+
+        else if (posicionX < -limiteX)
+        {
+            PLAYER.transform.position = new Vector3(-limiteX, PLAYER.transform.position.y, PLAYER.transform.position.z);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
+
+        }
+
+        if (posicionY > limiteY)
+        {
+            PLAYER.transform.position = new Vector3(PLAYER.transform.position.x, limiteY, PLAYER.transform.position.z);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 0);
+
+        }
+
+        else if (posicionY < -limiteY)
+        {
+            PLAYER.transform.position = new Vector3(PLAYER.transform.position.x, -limiteY, PLAYER.transform.position.z);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 0);
+        }
+
         //NORTE, SUD, ESTE y OESTE
-        
+
+
         //NORTE
         if (Input.GetKey(KeyCode.UpArrow))
-        {   
-            //AQUI DESBLOQUEAMOS CUALQUIER EJE
-            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+        {
+            GetComponent<Rigidbody2D>().velocity += new Vector2(0, GoUp) * Time.deltaTime;
 
-            //SI LA NAVE LLEGA AL LIMITE SUPERIOR LA ELOCIADA PASA A 0 Y BLOQUEAMOS EL MOVIMIENO EN Y          
-             if (transform.localPosition.y >= limiteY)
-                {
-                    finalSpeed = 0;
-
-                    GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
-
-                    //AQUI COMPROBAMOS QUE SI A PARTE DE ESTAR EN EL LIMITE SUPERIOR TAMBIEN ESTAMOS EN UN LIMETE DE DERECHA O IZQUIERDA 
-                    //BLOQUEAMOS TANTO EL MOVIMIENTO EN Y COMO EN X
-                    if (transform.localPosition.x <= -limiteX || transform.localPosition.x >= limiteX)
-                    {
-                        GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX |
-                              RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
-                    }
-                }
-                //SI NO ESTA LLEGANDO A NINGUN LIMITE finalSpeed PASA A TENER EL VALOR QUE DEBERIA
-                else
-                {
-                    finalSpeed = speed;
-
-                }
-            
-
-  
-            //AQUI IMPLEMENTAMOS EL MOVIMENT (PARECIDO A COMO LO HACIAMOS ANTES PERO CON UNA SOLA VARIABLE, totalSpeed, Y NO
-            // CON UNAVARIABLE PARA CADA DIRECCION COMO HACIAMOS ANTES)
-            GetComponent<Rigidbody2D>().velocity += new Vector2(0, finalSpeed)*Time.deltaTime;
-
-            if(GetComponent<Rigidbody2D>().velocity.y > GoUp)
+            if (GetComponent<Rigidbody2D>().velocity.y > GoUp)
             {
                 GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, GoUp);
             }
             buttonPressed = true;
-            
+
+            UpPressed = true;
+            DownPressed = false;
+            LeftPressed = false;
+            RightPressed = false;
         }
 
         //OESTE
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            //AQUI DESBLOQUEAMOS CUALQUIER EJE
-            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
-
-            if (transform.localPosition.x <= -limiteX)
-            {
-                finalSpeed = 0;
-               
-                GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
-
-                if (transform.localPosition.y >= limiteY || transform.localPosition.y <= -limiteY)
-                {
-                    GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX |
-                    RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
-                }           
-                
-            }
-            else
-            {
-                finalSpeed = speed;
-            }
-
-            GetComponent<Rigidbody2D>().velocity += new Vector2(-finalSpeed, 0)*Time.deltaTime;
+            GetComponent<Rigidbody2D>().velocity += new Vector2(GoLeft, 0) * Time.deltaTime;
 
             if (GetComponent<Rigidbody2D>().velocity.x < GoLeft)
             {
@@ -119,34 +125,17 @@ public class PLAYER_CONTROL : MonoBehaviour {
             }
 
             buttonPressed = true;
-            
+
+            UpPressed = false;
+            DownPressed = false;
+            LeftPressed = true;
+            RightPressed = false;
         }
 
         //SUD
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            //AQUI DESBLOQUEAMOS CUALQUIER EJE
-            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
-
-            if (transform.localPosition.y <= -limiteY)
-            {
-                finalSpeed = 0;
-                GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
-
-                if (transform.localPosition.x <= -limiteX || transform.localPosition.x >= limiteX)
-                {
-                    GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX |
-                    RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
-                }
-
-            }
-            else
-            {
-                finalSpeed = speed;
-
-            }
-            GetComponent<Rigidbody2D>().velocity += new Vector2(0, -finalSpeed) * Time.deltaTime;
-           
+            GetComponent<Rigidbody2D>().velocity += new Vector2(0, GoDown) * Time.deltaTime;
 
             if (GetComponent<Rigidbody2D>().velocity.y < GoDown)
             {
@@ -156,35 +145,17 @@ public class PLAYER_CONTROL : MonoBehaviour {
 
             buttonPressed = true;
 
-           
+            UpPressed = false;
+            DownPressed = true;
+            LeftPressed = false;
+            RightPressed = false;
         }
 
         //ESTE
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            //AQUI DESBLOQUEAMOS CUALQUIER EJE
-            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+            GetComponent<Rigidbody2D>().velocity += new Vector2(GoRigh, 0) * Time.deltaTime;
 
-            if (transform.localPosition.x >= limiteX)
-            {
-                finalSpeed = 0;
-
-                GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
-
-                if (transform.localPosition.y >= limiteY || transform.localPosition.y <= -limiteY)
-                {
-                    GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX |
-                    RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
-                }
-
-            }
-            else
-            {
-                finalSpeed = speed;
-            }
-
-            GetComponent<Rigidbody2D>().velocity += new Vector2(finalSpeed, 0) * Time.deltaTime;
-            
             if (GetComponent<Rigidbody2D>().velocity.x > GoRigh)
             {
                 GetComponent<Rigidbody2D>().velocity = new Vector2(GoRigh, GetComponent<Rigidbody2D>().velocity.y);
@@ -192,17 +163,166 @@ public class PLAYER_CONTROL : MonoBehaviour {
 
             buttonPressed = true;
 
-            
-        }  
-
-        //FRENAR
-        if (!buttonPressed)
-        {                
-
-           GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-           GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
-            
+            UpPressed = false;
+            DownPressed = false;
+            LeftPressed = false;
+            RightPressed = true;
         }
+
+        if (Input.GetKey(KeyCode.Space)) {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                Instantiate(flash, transform.position, transform.rotation);
+                PLAYER.transform.position = new Vector3(PLAYER.transform.position.x, PLAYER.transform.position.y + (Dash/40 *GetComponent<Rigidbody2D>().velocity.y), PLAYER.transform.position.z);
+                Instantiate(flash, transform.position, transform.rotation);
+            }
+
+            //OESTE
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                //Instantiate(flash, transform.position, transform.rotation);
+                PLAYER.transform.position = new Vector3(PLAYER.transform.position.x + (Dash / 40 * GetComponent<Rigidbody2D>().velocity.x), PLAYER.transform.position.y, PLAYER.transform.position.z);
+                //Instantiate(flash, transform.position, transform.rotation);
+            }
+
+            //SUD
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                //Instantiate(flash, transform.position, transform.rotation);
+                PLAYER.transform.position = new Vector3(PLAYER.transform.position.x, PLAYER.transform.position.y + (Dash / 40 * GetComponent<Rigidbody2D>().velocity.y), PLAYER.transform.position.z);
+               // Instantiate(flash, transform.position, transform.rotation);
+            }
+
+            //ESTE
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                //Instantiate(flash, transform.position, transform.rotation);
+                PLAYER.transform.position = new Vector3(PLAYER.transform.position.x + (Dash / 40 * GetComponent<Rigidbody2D>().velocity.x), PLAYER.transform.position.y, PLAYER.transform.position.z);
+                //Instantiate(flash, transform.position, transform.rotation);
+            }
+
+        }
+
+            //FRENAR
+            if (!buttonPressed)
+        {
+            // NORTE, SUD, ESTE Y OESTE
+
+            //NORTE
+            if (UpPressed == true && DownPressed == false && LeftPressed == false && RightPressed == false)
+            {
+                GetComponent<Rigidbody2D>().velocity -= new Vector2(0, DownBreak) * Time.deltaTime;
+
+
+                if (UpBreak <= 0)
+                {
+                    GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                    UpBreak = 40;
+
+                }
+            }
+
+            //OESTE
+            if (UpPressed == false && DownPressed == false && LeftPressed == true && RightPressed == false)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(LeftBreak, 0);
+                LeftBreak += 1;
+                if (LeftBreak >= 0)
+                {
+                    GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                    LeftBreak = 10;
+                }
+            }
+
+            //SUD
+            if (UpPressed == false && DownPressed == true && LeftPressed == false && RightPressed == false)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0, DownBreak);
+                DownBreak += 1;
+                if (DownBreak >= 0)
+                {
+                    GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                    DownBreak = -10;
+                }
+            }
+
+            //ESTE
+            if (UpPressed == false && DownPressed == false && LeftPressed == false && RightPressed == true)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(RightBreak, 0);
+                RightBreak -= 1;
+                if (RightBreak <= 0)
+                {
+                    GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                    RightBreak = -10;
+                }
+            }
+
+
+            //FRENAR MOVIMIENTOS EN DIAGONAL
+
+            //NORTE-ESTE
+            if (UpPressed == true && DownPressed == false && LeftPressed == false && RightPressed == true)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(RightBreak, UpBreak);
+                RightBreak -= 1;
+                UpBreak -= 1;
+                if (UpBreak <= 0 || RightBreak <= 0)
+                {
+                    GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                    UpBreak = 10;
+                    RightBreak = -10;
+                }
+            }
+
+            //NORTE-OESTE
+            if (UpPressed == true && DownPressed == false && LeftPressed == true && RightPressed == false)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(LeftBreak, UpBreak);
+                UpBreak -= 1;
+                LeftBreak += 1;
+                if (UpBreak <= 0 || LeftBreak >= 0)
+                {
+                    GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                    UpBreak = 10;
+                    LeftBreak = 10;
+                }
+            }
+
+            //SUD-OESTE
+            if (UpPressed == false && DownPressed == true && LeftPressed == true && RightPressed == false)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(LeftBreak, DownBreak);
+                DownBreak += 1;
+                LeftBreak += 1;
+                if (DownBreak >= 0 || LeftBreak >= 0)
+                {
+                    GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                    DownBreak = -10;
+                    LeftBreak = 10;
+                }
+            }
+
+            //SUD-ESTE
+            if (UpPressed == false && DownPressed == true && LeftPressed == false && RightPressed == true)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(RightBreak, DownBreak);
+                DownBreak += 1;
+                RightBreak -= 1;
+                if (DownBreak >= 0 || RightBreak <= 0)
+                {
+                    GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                    DownBreak = -10;
+                    RightBreak = -10;
+                }
+            }
+
+            else
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            }
+        }
+
 
 
 
@@ -210,10 +330,8 @@ public class PLAYER_CONTROL : MonoBehaviour {
         if (Input.GetButton("Fire1") && Time.time > nextFire)
         {
             nextFire = Time.time + fireRate;
-            Instantiate(shot,shotSpawn.position,shotSpawn.rotation);
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
         }
-
-
 
     }
 }
