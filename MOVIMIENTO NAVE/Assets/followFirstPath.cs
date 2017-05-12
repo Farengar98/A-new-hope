d32f1c0ed381f4aee890ce112ae33963f8939bab
Unknown <ferranromerogallardo@enti.cat>
2017-05-12 13:12:33 +0200

    Path

---
+++ MOVIMIENTO NAVE/Assets/followFirstPath.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followFirstPath : MonoBehaviour {
+
+	public float vel = 20f;
+	public string nameOfPath = "First 1";
+
+	// Use this for initialization
+	void Awake()
+	{
+		transform.FollowPath (nameOfPath, vel, Mr1.FollowType.PingPong);
+	}
+}
