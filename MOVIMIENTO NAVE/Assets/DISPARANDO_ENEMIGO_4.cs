3bbc5a41067ee2ab24e08d4cddaf5f6c3b9b6518
Unknown <Sergio.escuder5@gmail.com>
2017-06-06 22:26:52 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ MOVIMIENTO NAVE/Assets/DISPARANDO_ENEMIGO_4.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DISPARANDO_ENEMIGO_4 : MonoBehaviour {
+
+    public GameObject shot;
+    public GameObject shotUp;
+    public GameObject shotDown;
+    public Transform shootSpawn;
+    public Transform shotSpawnUp;
+    public Transform shotSpawnDown;
+    public float FireRate;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(ShootSpawn());
+    }
+
+    // Update is called once per frame
+    IEnumerator ShootSpawn()
+    {
+        while (true)
+        {
+            Instantiate(shot, shootSpawn.position, shootSpawn.rotation);
+            Instantiate(shotUp, shotSpawnUp.position, shotSpawnUp.rotation);
+            Instantiate(shotDown, shotSpawnDown.position, shotSpawnDown.rotation);
+
+            yield return new WaitForSeconds(FireRate);
+        }
+    }
+}
---
+++ MOVIMIENTO NAVE/Assets/DISPARO_ENEMIGO_4_L_UP.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DISPARO_ENEMIGO_4_L_UP : MonoBehaviour {
+    public float speed;
+
+    private Rigidbody2D rig;
+
+    void Awake()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        rig.velocity = new Vector2(-speed, speed / 3);
+    }
+}
